bfc32099019214ce1cb20aeba0d27b647303512c
VU BINH CHUONG <chuongvbgch190772@fpt.edu.vn>
2022-09-24 17:32:57 +0700

    ok

--- index.js
+++ index.js
@@ -13,14 +13,23 @@ app.set("views", path.join(__dirname, "views") );
 app.set("view engine", "hbs");
 
 app.get("/", (req,res)=>{
-    res.render("index");
+    var name = "Cloud computing";
+    var code = 1644;
+    var semester = "Fall 2022";
+    // pass tu back-end sang front-end
+    //cu phap : name (data): value(date)
+    // gui value va nhan voi ten la name
+    res.render("index", {name: name, code: code, semester: semester});
+    //  res.render("index", {name:"Cloud computing", code: 1644 , semester: "Fall 2022"}
 });
 
 app.get("/mobile", (req,res)=>{
-    res.render("mobile");
+    var list = ["iPhone", "Samsung", "Oppo", "LG"];
+    res.render("mobile", {mobiles: list});
 });
 app.get("/laptop", (req,res)=>{
-    res.render("laptop");
+    var list = ["Dell", "HP", "Sonny", "Accer"];
+    res.render("laptop", {laptops: list });
 });
 
 //listen port de chay server
